3366ceabdf76c274ced71b65f2203b43c34b4850
Renee <reneesaethre@gmail.com>
2022-09-07 16:43:31 +0200

    added pie chart for relative dwell times

--- visualisations.py
+++ visualisations.py
@@ -1,4 +1,5 @@
 # load packages
+from cmath import pi
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -20,6 +21,19 @@ def vis_ooi_metrics(df, outputpath, trialname, specification):
         plt.clf()
 
     # cake plot for Relative Dwelltime [%]:
+
+    # only plot if greater than 0% 
+    piedata = df.loc['Relative Dwelltime [%]'] 
+    piedata = piedata[piedata!=0]
+
+    # make pie chart
+    savepath=outputpath /'{}_pieplot_{}.jpg'.format('Relative Dwelltime [%]', specification)
+    number_cols = len(piedata)
+    clrs = sns.color_palette('pastel')[0:number_cols]
+    lbls = piedata.index
+    plt.pie(piedata, labels = lbls, colors = clrs, autopct='%.0f%%' )
+    plt.title('Relative Dwelltime [%] per OOI ({})'.format(specification), fontsize = 16, pad = 20, weight = 'bold')
+    plt.savefig(savepath, bbox_inches = 'tight', dpi = 300)
 
  
 
